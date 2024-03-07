b4976651d2c094e7a7598d444c1d7652e9458396
Rizky Ramadhan <rizkyramadhan@Rizkys-MacBook-Pro.local>
2024-03-07 16:53:59 +0700

    feat: stretch the page to 100 per page and add info provincs, regency, district

--- model/lembaga.js
+++ model/lembaga.js
@@ -1,10 +1,13 @@
 var db = require('../config/database.js');
 
-exports.saveLembaga = async function (nama_lembaga, nama_pimpinan, phone) {
+exports.saveLembaga = async function (nama_lembaga, nama_pimpinan, phone, provinsi, kota_kabupaten, kecamatan) {
 	var lembaga_data = {
 		'nama_lembaga' : nama_lembaga,
 		'nama_pimpinan' : nama_pimpinan,
-		'phone' : phone
+		'phone' : phone,
+		'provinsi' : provinsi,
+		'kota_kabupaten': kota_kabupaten,
+		'kecamatan' : kecamatan
 	};
 
 	db.get().query('INSERT INTO lembaga SET ?', [lembaga_data], function(err, result) {
